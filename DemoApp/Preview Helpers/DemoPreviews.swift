c9ceb07caf1845a5a8c7b216d447de2692d6bca3
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-09-09 07:32:22 -0600

    first commit

---
+++ DemoApp/Preview Helpers/DemoPreviews.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SwiftUI
+
+struct DemoPreviews_Previews: PreviewProvider {
+    static var previews: some View {
+        UIViewControllerPreview {
+            let vc = ViewController()
+            return vc
+        }
+    }
+}
---
+++ DemoApp/Preview Helpers/SwiftUIPreviewViewController.swift
@@ -0,0 +1,21 @@
+import Foundation
+import UIKit
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+    let viewController: ViewController
+
+    init(_ builder: @escaping () -> ViewController) {
+        viewController = builder()
+    }
+
+    func makeUIViewController(context: Context) -> ViewController {
+        viewController
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: UIViewControllerRepresentableContext<UIViewControllerPreview<ViewController>>) {
+        return
+    }
+}
+#endif
